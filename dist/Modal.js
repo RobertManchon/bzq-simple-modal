eb9aac0fa0f9bf180c76efd4ff42b37da0d1eccf
RManchon <erasme666@gmail.com>
2022-01-21 08:02:51 +0100

    version 2

---
+++ dist/Modal.js
@@ -0,0 +1,152 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireWildcard(require("react"));
+
+var _useClickOutsideRef = _interopRequireDefault(
+    require("./useClickOutsideRef")
+);
+
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
+}
+
+function _getRequireWildcardCache(nodeInterop) {
+    if (typeof WeakMap !== "function") return null;
+    var cacheBabelInterop = new WeakMap();
+    var cacheNodeInterop = new WeakMap();
+    return (_getRequireWildcardCache = function (nodeInterop) {
+        return nodeInterop ? cacheNodeInterop : cacheBabelInterop;
+    })(nodeInterop);
+}
+
+function _interopRequireWildcard(obj, nodeInterop) {
+    if (!nodeInterop && obj && obj.__esModule) {
+        return obj;
+    }
+    if (obj === null || (typeof obj !== "object" && typeof obj !== "function")) {
+        return { default: obj };
+    }
+    var cache = _getRequireWildcardCache(nodeInterop);
+    if (cache && cache.has(obj)) {
+        return cache.get(obj);
+    }
+    var newObj = {};
+    var hasPropertyDescriptor =
+        Object.defineProperty && Object.getOwnPropertyDescriptor;
+    for (var key in obj) {
+        if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) {
+            var desc = hasPropertyDescriptor
+                ? Object.getOwnPropertyDescriptor(obj, key)
+                : null;
+            if (desc && (desc.get || desc.set)) {
+                Object.defineProperty(newObj, key, desc);
+            } else {
+                newObj[key] = obj[key];
+            }
+        }
+    }
+    newObj.default = obj;
+    if (cache) {
+        cache.set(obj, newObj);
+    }
+    return newObj;
+}
+
+function Modal({
+                   isActive,
+                   setState,
+                   modalContent,
+                   backgroundStyle,
+                   contentStyle,
+                   exitBtn,
+                   exitBtnStyle,
+                   refresh,
+                   persist
+               }) {
+    const modalRef = (0, _useClickOutsideRef.default)(() => {
+        if (isActive && !persist) setState(false);
+    }); //handle close modal by esc button
+
+    const handleEscape = (0, _react.useCallback)((e) => {
+        if (e.keyCode === 27) setState(false);
+    });
+    (0, _react.useEffect)(() => {
+        if (isActive) {
+            document.addEventListener("keydown", handleEscape, false);
+        }
+
+        return () => {
+            document.removeEventListener("keydown", handleEscape, false);
+        };
+    }, [handleEscape, isActive]);
+    const backStyle = {
+        display: isActive ? "block" : "none",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        width: "100vw",
+        height: "100vh",
+        zIndex: 9999,
+        backgroundColor: "rgba(255,255,255,0.35)",
+        textAlign: "center",
+        ...backgroundStyle
+    };
+    const innerStyle = {
+        maxWidth: "20%",
+        position: "relative",
+        border: "3px solid red",
+        borderRadius: 10,
+        backgroundColor: "white",
+        padding: 20,
+        marginLeft: "50%",
+        marginTop: "25%",
+        transform: "translate(-50%, -50%)",
+        ...contentStyle
+    };
+    const exitStyle = {
+        position: "absolute",
+        top: 5,
+        right: 15,
+        cursor: "pointer",
+        ...exitBtnStyle
+    };
+
+    const handleClickExit = () => {
+        setState(false);
+
+        if (refresh) {
+            window.location.reload();
+        }
+    };
+
+    return /*#__PURE__*/ _react.default.createElement(
+        "div",
+        {
+            style: backStyle
+        },
+        /*#__PURE__*/ _react.default.createElement(
+            "div",
+            {
+                style: innerStyle,
+                ref: modalRef
+            },
+            modalContent,
+            /*#__PURE__*/ _react.default.createElement(
+                "div",
+                {
+                    style: exitStyle,
+                    onClick: handleClickExit
+                },
+                exitBtn ? exitBtn : "X"
+            )
+        )
+    );
+}
+
+var _default = Modal;
+exports.default = _default;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import Example from './Example';
 
 
 ReactDOM.render(
-	<Example />,
-
-	document.getElementById('root')
-);
+	<Example />
+	, 
+	document.getElementById("root"));
